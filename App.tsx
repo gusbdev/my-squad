f79de97aba52faa4c9496f25942fb1e20f1d9e27
Gustavo Barbosa <gusbdev@gmail.com>
2021-02-07 13:12:39 -0300

    Installation and config of dependencies

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Routes from './src/routes';
+
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
-  return <></>;
+  return <Routes />;
 };
 
 export default App;
